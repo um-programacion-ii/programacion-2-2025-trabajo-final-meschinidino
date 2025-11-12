caf891dc84e805d1016855577935aceb268e21d7
meschinidino <meschinidino@gmail.com>
2025-11-12 14:35:14 -0300

    Proyecto Spring Boot creado - Dependencias Maven configuradas - application.yml configurado - Estructura de carpetas creada - Aplicación levanta correctamente

---
+++ backend/src/test/java/com/eventos/sistemaeventos/SistemaEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventos.sistemaeventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaEventosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
